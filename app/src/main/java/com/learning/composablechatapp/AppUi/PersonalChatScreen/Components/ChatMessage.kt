90a394b68dac5f6d932ae2b5837dd566337fc5d5
Nati892 <netanel892@gmail.com>
2022-05-22 20:43:40 +0300

    more custom' for chat

--- app/src/main/java/com/learning/composablechatapp/AppUi/PersonalChatScreen/Components/ChatMessage.kt
+++ app/src/main/java/com/learning/composablechatapp/AppUi/PersonalChatScreen/Components/ChatMessage.kt
@@ -1,13 +1,16 @@
 package com.learning.composablechatapp.AppUi
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.learning.composablechatapp.data.Repos.MessageData
@@ -18,28 +21,37 @@ fun ChatMessage(
 ) {
 
     Row(Modifier.padding(3.dp)) {
-        var BoxColor = Color.Cyan
-
+        var boxColor = Color.Cyan
+        val ClipDp = 8.dp
+        var clipTopStart = 0.dp
+        var clipTopEnd = ClipDp
         if (MessageData.SenderId != 0) {// case of received message
-            BoxColor = Color.Green
+            boxColor = Color.Green
             Spacer(modifier = Modifier.weight(1f))
+            clipTopEnd = 0.dp;
+            clipTopStart = ClipDp
         }
 
         Box(
             Modifier
-                .background(BoxColor)
-                .clip(RoundedCornerShape(topEnd = 16.dp))
+                .clip(
+                    RoundedCornerShape(
+                        topStart = clipTopStart,
+                        topEnd = clipTopEnd,
+                        bottomEnd = ClipDp,
+                        bottomStart = ClipDp
+                    )
+                )
+                .background(boxColor)
         ) {
             Text(
                 modifier = Modifier.padding(horizontal = 3.dp),
-                text = "${MessageData.MessageText}",
+                text = "${MessageData.MessageText.trim()}",
                 textAlign = if (MessageData.SenderId != 0) TextAlign.Right else TextAlign.Left
+               , fontWeight = FontWeight(300)
             )
         }
-
     }
-
-
 }
 
 
