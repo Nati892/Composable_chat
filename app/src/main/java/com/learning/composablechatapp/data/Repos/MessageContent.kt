27d063a403e2c846ba6b78b4f7e2a83e3b70a2b9
Nati892 <netanel892@gmail.com>
2022-05-29 22:23:18 +0300

    remade Messagedata etc, left TODOs

---
+++ app/src/main/java/com/learning/composablechatapp/data/Repos/MessageContent.kt
@@ -0,0 +1,6 @@
+package com.learning.composablechatapp.data.Repos
+
+class DataMessageContent {//TODO Data inteface
+
+
+}
--- app/src/main/java/com/learning/composablechatapp/data/Repos/MessageData.kt
+++ app/src/main/java/com/learning/composablechatapp/data/Repos/MessageData.kt
@@ -3,11 +3,21 @@ package com.learning.composablechatapp.data.Repos
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+enum class messageType{
 
+}
 class MessageData(
-    val MessageText: String,
-    val SenderId: Int= 0
-)
+    val MessageId:Long=0,
+    val SenderId: Long=0,
+    val Receiver: Long=0,
+    val MessageText: String="",
+    val DateTimeObj_sent: Object?=null,
+    val DateTimeObj_receivedByServer: Object?=null,
+    val DateTimeObj_receivedByUser: Object?=null,
+    val DateTimeObj_seenByUser: Object?=null,
+    val Forwarded:Boolean=false,
+    val Content:Object?=null
+    )//TODO DateTimeObjObj, and content obj
 {
-    var checked by mutableStateOf(SenderId)
+
 }
